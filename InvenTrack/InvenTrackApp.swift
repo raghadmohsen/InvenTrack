87ce0a8ddd0ee637128d6f06c254390ddb410ad3
Reefsaeed <reefsaeed@hotmail.com>
2024-10-06 09:29:18 +0300

    Initial Commit

---
+++ InvenTrack/InvenTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvenTrackApp.swift
+//  InvenTrack
+//
+//  Created by Reef Saeed on 06/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct InvenTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
