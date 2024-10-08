edcee633704b6993135d0bb7a01697eabeb26ce8
arwaq1 <arwaalqhtani52@gmail.com>
2024-10-08 10:11:08 +0300

    Update

---
+++ InvenTrack/LowStocks.swift
@@ -0,0 +1,76 @@
+
+import SwiftUI
+import SwiftData
+
+struct LowStocks: View {
+    @Query(sort: \DataItem.id) var dataitem: [DataItem]
+    @Environment(\.modelContext) private var context
+    
+    var body: some View {
+        NavigationStack{
+            VStack {
+                List {
+                    let outOfStockItems = dataitem.filter { $0.Quantity >= $0.MinQuantity }
+                    
+                    ForEach(outOfStockItems) { item in
+                        itemell(items: item)
+                    }
+                    listRowBackground(
+                        Capsule()
+                            .fill(Color.gray.opacity(0.1))
+                            .padding(.horizontal, 7)
+                        )
+                }
+            }
+            .navigationTitle("In Stock")
+        }
+    }
+    
+    struct ItemCell: View {
+        let items: DataItem
+        
+        var body: some View {
+            VStack(alignment: .leading) {
+                Text(items.Name)
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .foregroundColor(.primary)
+                    .padding(.bottom, 1)
+                
+                Text("Quantity: \(items.Quantity)")
+                    .font(.footnote)
+                    .foregroundColor(.secondary)
+            }
+            .padding()
+        }
+    }
+    
+}
+
+struct iteCmell: View {
+    let items: DataItem
+    var body: some View {
+ 
+        VStack {
+            VStack(spacing: 20) {
+                        VStack(alignment: .leading) {
+                            Text(items.Name)
+                                .font(.title3)
+                                .fontWeight(.bold)
+                                .foregroundColor(.primary)
+                                .padding(.bottom, 1)
+                            
+                            Text("Quantity: \(items.Quantity)")
+                                .font(.footnote)
+                                .foregroundColor(.secondary)
+                        }
+                    
+                .padding()
+                //.background(Color.gray.opacity(0.1))
+                .frame(width: 370)
+                .cornerRadius(30)
+            }
+        }
+    }
+}
+
